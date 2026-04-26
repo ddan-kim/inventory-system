80c4508f2f54c84d40a10eb6ae30c9e6ff64cf66
Mark-Kim1 <mark@tourmktg.co.kr>
2026-04-27 00:06:22 +0900

    docs: ERD 추가 및 README 최신화
    - 확장 ERD 다이어그램 추가 (docs/erd-extended.png)
    - Category 관련 미사용 코드 제거 (ErrorCode, 패키지)

--- inventory-api/src/main/java/com/inventory/api/category/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inventory.api.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.inventory.domain.category.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
